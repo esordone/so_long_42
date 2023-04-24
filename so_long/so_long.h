4f0d32c24417d9332ad52f068f0e5b8037f04661
Estrella Ordonez Aguilar <esordone@car13s3.42barcelona.com>
2023-04-24 12:26:32 +0200

    jkbfvkjbfvkjbefvjk

---
+++ so_long/so_long.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: esordone <esordone@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/24 12:22:03 by esordone          #+#    #+#             */
+/*   Updated: 2023/04/24 12:23:27 by esordone         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <stdio.h>
+# include <limits.h>
+# include "./libft/libft.h"
+
+int so_long(int argc, char **argv);
+
+#endif
