07202e7b6cf57f74c95022c3fb73c0bf58aa9bb2
Estrella Ordonez Aguilar <esordone@car13s3.42barcelona.com>
2023-04-24 15:47:57 +0200

    mapa

--- so_long/so_long.h
+++ so_long/so_long.h
@@ -6,7 +6,7 @@
 /*   By: esordone <esordone@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 12:22:03 by esordone          #+#    #+#             */
-/*   Updated: 2023/04/24 12:23:27 by esordone         ###   ########.fr       */
+/*   Updated: 2023/04/24 14:14:27 by esordone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,13 @@
 
 int so_long(int argc, char **argv);
 
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color);
 #endif
--- so_long/source/so_long.c
+++ so_long/source/so_long.c
@@ -6,20 +6,44 @@
 /*   By: esordone <esordone@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 12:19:01 by esordone          #+#    #+#             */
-/*   Updated: 2023/04/24 12:20:10 by esordone         ###   ########.fr       */
+/*   Updated: 2023/04/24 15:39:54 by esordone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
 
 //int main(int argc, char **argv)
+//
+//I should ALWAYS calculate the memory offset using the line length 
+//set by mlx_get_data_addr with this formula:
+//
+//int offset = (y * line_length + x * (bits_per_pixel / 8));
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
 
 int main()
 {
 	void	*mlx;
 	void	*mlx_win;
+	t_data	img;
+	int		i = 400;
 
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "**** QUARANTAMAULA ****");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	//add es l'adreca del primer pixel;
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	while (i < 1000)
+	{
+		my_mlx_pixel_put(&img, i, 500, 0x00FF0000);
+		i++;
+	}
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 }
