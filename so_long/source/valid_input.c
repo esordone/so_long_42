f9cec4ca23b2795cfe49c7847d264bc4938f9c4c
Estrella Ordonez Aguilar <esordone@cbr8s2.42barcelona.com>
2023-05-03 15:24:03 +0200

    valid map

--- so_long/source/valid_input.c
+++ so_long/source/valid_input.c
@@ -12,27 +12,60 @@
 
 #include "../so_long.h"
 
-int	valid_input(char **argv, char *file)
+int	valid_input_map(char **argv, char *file)
 {
 	if ((walls_map(argv, file) != 1) || (rectangular_map(argv, file) != 1))
 	{
-		ft_printf("The map is not closed or is not a rectangle.\n");
+		ft_printf("Error\nThe map is not closed or is not a rectangle.\n");
 		return (0);
 	}
 	if ((exit_on_map(argv, file) != 1))
 	{
-		ft_printf("There is no exit in the map.\n");
+		ft_printf("Error\nThere is no exit in the map.\n");
 		return (0);
 	}
 	if ((start_on_map(argv, file) != 1))
 	{
-		ft_printf("There is no starting position in the map.\n");
+		ft_printf("Error\nThere is no starting position in the map.\n");
 		return (0);
 	}
 	if ((object_on_map(argv, file) != 1))
 	{
-		ft_printf("There are no collectibles on the map.\n");
+		ft_printf("Error\nThere are no collectibles on the map.\n");
 		return (0);
 	}
+	return (1);
+}
+
+/*int	valid_double_input(char **map, char *file)
+{
+	t_map	data;
+
+	init_struct(&data);
+	if(check_doubles(map,file, data.start) != 1)
+	{
+		ft_printf("There are more than one starting position in the map.\n");
+		return (0);
+	}
+	if(check_doubles(map,file, data.exit) != 1)
+	{
+		ft_printf("There are more than one exit in the map.\n");
+		return (0);
+	}
+	return (1);
+}*/
+
+int	valid_input(char **argv, char *file)
+{
+	t_map	data;
+
+	init_struct(&data);
+	if (valid_input_map(argv, file) == 0)
+		return (0);
+	/*if (valid_double_input(argv, file) == 0)
+	{
+		ft_printf("Error\n");
+		return (0);
+	}*/
 	return (1);
 }
--- so_long/source/valid_input_utils3.c
+++ so_long/source/valid_input_utils3.c
@@ -12,6 +12,22 @@
 
 #include "../so_long.h"
 
+int	is_double_in_map(char *map, char object, int len)
+{
+	int	i;
+	int	check;
+
+	i = 1;
+	check = 0;
+	while (i < len && map[i] != '\0')
+	{
+		if (map[i] == object)
+			check++;
+		i++;
+	}
+	return (check);
+}
+
 int	object_on_map(char **map, char *file)
 {
 	t_map	data;
@@ -38,6 +54,9 @@ int	object_on_map(char **map, char *file)
 	check_last_line_map(map, file, data.colect);
 	return (0);
 }
+
+
+
 
 		//printf("/////// = %c\n", map[data.y + 1][data.x + 1]);
 		//printf("estas aci ara = %c\n", map[data.y + 1][data.x + 1]);
